a0f4d01b33fe234b880ecdf6e9cd4e72ad3d9e3e
Pandunanda Primadifani Kafah <pandukyouka@gmail.com>
2024-05-07 20:17:28 +0700

    Create Lottery machine.py

---
+++ 7Kyu/Lottery machine.py
@@ -0,0 +1,6 @@
+def lottery(s):
+    s = ''.join(filter(str.isdigit, s))
+    s = ''.join(sorted(set(s), key=lambda x: s.index(x)))
+    return s if s else 'One more run!'
+
+print(lottery('jhgdaushga2727'))
