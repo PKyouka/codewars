2a1bfbc99da8341bdf0e620ba3701af4ed3b166c
Pandunanda Primadifani Kafah <pandukyouka@gmail.com>
2024-06-14 15:47:01 +0700

    Create Regular Ball Super Ball.py

---
+++ 8Kyu/Regular Ball Super Ball.py
@@ -0,0 +1,9 @@
+
+class SuperBall:
+    def __init__(self, ball_type="regular"):
+        if ball_type == "super":
+            self.ball_type = "super"
+        else:
+            self.ball_type = "regular"
+
+
